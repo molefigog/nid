2129c15283aac48c94366fb8d0bc7aa55d484e8d
Molefi <sebatadorothy@gmail.com>
2025-05-14 01:48:37 +0200

    gee

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -19,8 +19,33 @@ class Product extends Model
         'category_id',
         'opening_stock',
         'closing_stock',
-        'closing_stock_updated_at'
+        'closing_stock_updated_at',
+        'sc'
     ];
+    protected static function booted()
+    {
+        static::creating(function ($product) {
+            $product->sc = self::generateShortcode($product->name);
+        });
+    }
+
+    public static function generateShortcode($name)
+    {
+        preg_match('/([A-Za-z]+)\s?([A-Za-z]+)?\s?(\d+[\.\*]?\d*)(mm)?\s?([A-Za-z]+)?/', $name, $matches);
+
+        // Log products that don't match
+        if (empty($matches)) {
+            \Log::error("Failed to generate shortcode for product: {$name}");
+        }
+
+        // Safe checks to avoid warnings when matches are missing
+        $initials = isset($matches[1]) && strlen($matches[1]) > 0 ? strtoupper($matches[1][0]) : '';
+        $secondWord = isset($matches[2]) && strlen($matches[2]) > 0 ? strtoupper($matches[2][0]) : '';
+        $number = isset($matches[3]) ? $matches[3] : '';
+        $suffix = isset($matches[5]) && strlen($matches[5]) > 0 ? strtoupper($matches[5]) : '';
+
+        return "{$initials}{$secondWord}{$number}{$suffix}";
+    }
 
     public function category()
     {
